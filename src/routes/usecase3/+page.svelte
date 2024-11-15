ae0c74ac22f003061db7b5f08cc2b1eff4b81590
sairammobius <sairam.l@mobiusdtaas.ai>
2024-11-15 15:23:29 +0530

    initial commit

---
+++ src/routes/usecase3/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    await import("htmx.org");
+  });
+</script>
+
+<h1>Fetching Server Response using "hx-get" on div</h1>
+
+<div hx-get="/api/submit" hx-trigger="click[ctrlKey]" style="cursor:pointer;">
+  <b>Control Click Me</b>
+</div>
